5053181087e617d443aab2a86b559663da927c0d
Danny <danny@wednesdaytalent.com>
2023-09-09 14:22:06 -0400

    https

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    {/*    <script
+      async
+      src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places&callback=initMap"
+    >
+    </script>*/}
     <App />
   </React.StrictMode>
 );
